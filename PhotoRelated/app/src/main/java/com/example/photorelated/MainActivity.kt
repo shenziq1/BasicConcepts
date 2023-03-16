39c360d9f556e46538be95b7ca5113a841c6c6d7
shenziq2 <zshen331@gatech.edu>
2023-03-16 19:35:17 -0400

    photoRelated done

--- PhotoRelated/app/src/main/java/com/example/photorelated/MainActivity.kt
+++ PhotoRelated/app/src/main/java/com/example/photorelated/MainActivity.kt
@@ -7,16 +7,18 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
 import com.example.photorelated.ui.theme.PhotoRelatedTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,41 +33,96 @@ class MainActivity : ComponentActivity() {
                     val imagesSelected = remember {
                         mutableStateListOf<Uri>()
                     }
-                    OpenGalleryScreen(imagesSelected){ imagesSelected.add(it)}
+                    val testStrings = remember {
+                        mutableStateListOf<String>()
+                    }
+                    
+//                    LazyColumn(){
+//                        item{
+//                            OpenGalleryButton { imagesSelected.add(it) }
+//                        }
+//                        item{
+//                            OpenCameraButton { imagesSelected.add(it) }
+//                        }
+//                        item{
+//                            TestStringButton {testStrings.add(it)}
+//                        }
+//                        items(items = imagesSelected){
+//                            Image(
+//                                painter = rememberAsyncImagePainter(it),
+//                                contentDescription = null
+//                            )
+//                        }
+//                        items(items = testStrings){
+//                            Text(text = it)
+//                        }
+//                    }
+                    Column() {
+                        OpenGalleryButton() { imagesSelected.add(it) }
+                        OpenCameraButton() { imagesSelected.add(it) }
+                        imagesSelected.forEach {
+                            AsyncImage(
+                                model = it,
+                                modifier = Modifier.fillMaxWidth(),
+                                contentDescription = "Selected image",
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun OpenGalleryScreen(imagesSelected: List<Uri>, onImageSelected: (Uri) -> Unit) {
-
+fun OpenGalleryButton(onImageSelected: (Uri) -> Unit) {
     val galleryLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) {
             onImageSelected(it)
         }
-
-    Scaffold { paddingValues ->
-        Column(modifier = Modifier.padding(paddingValues)) {
-            if (imagesSelected != listOf(Uri.EMPTY))
-                imagesSelected.forEach {
-                    Image(painter = rememberAsyncImagePainter(it), contentDescription = null)
-                }
-            Button(onClick = { galleryLauncher.launch("image/*") }) {
-                Text(text = "open gallery")
-            }
-        }
-
+    Button(onClick = {
+        galleryLauncher.launch("image/*")
+    }) {
+        Text(text = "open gallery")
     }
-
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    PhotoRelatedTheme {
-//        OpenGalleryScreen()
-//    }
-//}
+@Composable
+fun OpenCameraButton(onPhotoTaken: (Uri) -> Unit) {
+    var hasImage by remember { mutableStateOf(false) }
+    val cameraLauncher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+        hasImage = it
+    }
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
+    Button(
+        modifier = Modifier.padding(top = 16.dp),
+        onClick = {
+            coroutineScope.launch {
+                val uri = ComposeFileProvider.getImageUri(context)
+                imageUri = uri
+                hasImage = false
+                cameraLauncher.launch(uri)
+            }
+        },
+    ) {
+        Text(
+            text = "take photo"
+        )
+    }
+    if (hasImage && imageUri != null) {
+        onPhotoTaken(imageUri!!)
+    }
+}
+
+@Composable
+fun TestStringButton(onButtonClicked: (String) -> Unit){
+    Button(onClick = { onButtonClicked("test") }) {
+        Text(text = "test")
+    }
+    
+}
